3edea5a8b25dcf199bf6ed228662479f5d6fd8ed
smsmth <kensee327@gmail.com>
2026-05-30 02:06:13 +0900

    Initial Commit

---
+++ Design Hub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Design Hub
+//
+//  Created by 栗本健正 on 2026/05/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Design Hub/Design_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_HubApp.swift
+//  Design Hub
+//
+//  Created by 栗本健正 on 2026/05/30.
+//
+
+import SwiftUI
+
+@main
+struct Design_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
